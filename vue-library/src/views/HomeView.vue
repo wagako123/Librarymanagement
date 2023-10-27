06899cbc86cb3a577cb9abba0910940ecfb2664f
unknown <brianwagako@gmail.com>
2023-10-27 11:41:31 +0300

    adding vuefity

--- vue-library/src/views/HomeView.vue
+++ vue-library/src/views/HomeView.vue
@@ -1,9 +1,26 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <div class="bg-dark">
+      <v-container class="p-4">
+        <h2 class="text-center text-white">Vue (pinia) crud</h2>
+      </v-container>
+    </div>
+    <v-container class="my-3 ">
+     
+        <v-card>
+          <div>
+          <v-card-title>Authors(0)</v-card-title>
+          <v-btn text color="success">New</v-btn>
+        </div>
+
+        </v-card>
+    </v-container>
+  </div>
 </template>
+
+<style>
+.bg-dark{
+  background-color: black;
+}
+</style>
